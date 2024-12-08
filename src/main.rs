c7e98e4a9e7f9cf0bc5531f7d1137ecb003b88be
Henry <101425175+ehaki@users.noreply.github.com>
2024-12-07 23:11:56 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use colored::Colorize;
+
+fn main() {
+    println!(" ");
+    println!("    {}    ", "#".green());
+    println!("   {}    {}", "###".green(), "Merry".red());
+    println!("  {}     {}", "#####".green(), "Christmas!".red());
+    println!(" {} ", "#######".green());
+    println!("    {}    ", "|".yellow());
+    println!(" ");
+}
